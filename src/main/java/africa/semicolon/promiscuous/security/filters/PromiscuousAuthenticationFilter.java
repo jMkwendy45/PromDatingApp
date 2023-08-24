6786c35c063c0720a89850c98029b70463395cad
Duru Oluchi M <duwend46@gmail.com>
2023-08-24 18:44:23 +0100

    aunth3

--- src/main/java/africa/semicolon/promiscuous/security/filters/PromiscuousAuthenticationFilter.java
+++ src/main/java/africa/semicolon/promiscuous/security/filters/PromiscuousAuthenticationFilter.java
@@ -6,20 +6,34 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
 import java.io.InputStream;
-
+@AllArgsConstructor
 public class PromiscuousAuthenticationFilter  extends  UsernamePasswordAuthenticationFilter  {
+
+    private final AuthenticationManager authenticationManager;
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
+            //Extract auth credential from the request
             InputStream inputStream =request.getInputStream();
-             objectMapper.readValue(inputStream, LoginRequest.class);
+            //use the object mapper to get requet
+        LoginRequest loginRequest =  objectMapper.readValue(inputStream, LoginRequest.class);
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+        //2 create an auth Object that is not yet authenticated
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email,password);
+        //delegate authentication responsiblity of the auth object to the auth manager
+            authenticationManager.authenticate(authentication)
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
