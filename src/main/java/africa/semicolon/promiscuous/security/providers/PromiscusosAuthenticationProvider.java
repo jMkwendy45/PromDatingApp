3c5acf4b5fe42af7e5ed8f2a5b7e32d5b62da344
Duru Oluchi M <duwend46@gmail.com>
2023-08-24 19:11:04 +0100

    aunth

--- src/main/java/africa/semicolon/promiscuous/security/providers/PromiscusosAuthenticationProvider.java
+++ src/main/java/africa/semicolon/promiscuous/security/providers/PromiscusosAuthenticationProvider.java
@@ -1,17 +1,25 @@
 package africa.semicolon.promiscuous.security.providers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component
+@AllArgsConstructor
 public class PromiscusosAuthenticationProvider  implements     AuthenticationProvider{
+   private final UserDetailsService userDetailsService;
+   private final PasswordEncoder passwordEncoder;
 
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
         return null;
     }
 
